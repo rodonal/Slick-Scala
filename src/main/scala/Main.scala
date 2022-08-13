5c28ea20a3d689e8e2481d7f050c61d1c8da32ec
Redion Xhepa <redionxhepa@outlook.com>
2022-08-13 16:01:51 +0200

    Sql queries with Slick

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 package org.slick.tutorial
 
+import slick.jdbc.GetResult
+
 import java.time.LocalDate
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
-
 
 object PrivateExecutionContext {
 
@@ -19,10 +20,12 @@ object Main {
   import slick.jdbc.PostgresProfile.api._
   import PrivateExecutionContext._
 
+  val shawShankRedemption = Movie(1L, "ShawSHank Redemption", LocalDate.of(1994, 9, 23), 162)
+  val theMatrix = Movie(2L, "THe Matrix", LocalDate.of(1999, 12, 1), 140)
+  val redionXhepa = Actor(1L, "Redion Xhepa")
 
-  val shawShankRedemption = Movie(1, "ShawSHank Redemption", LocalDate.of(1994, 9, 23), 162)
-  val theMatrix = Movie(2, "THe Matrix", LocalDate.of(1999, 12, 1), 150)
-
+  val theMatrix2 = Movie(2L, "THe Matrix 2", LocalDate.of(2002, 11, 1), 100)
+  val redionXhepa2 = Actor(1L, "Redion Xhepa 2")
 
   def demoInsertMovie(): Unit = {
     val queryDescription = SlickTables.movieTable += theMatrix
@@ -33,34 +36,49 @@ object Main {
       case Failure(ex) => println(s"Faile with error $ex")
 
     }
-    Thread.sleep(10000)
+    Thread.sleep(5000)
   }
 
+  def demoInsertActor(): Unit = {
+    val queryDescription = SlickTables.actorTable ++= Seq(redionXhepa)
+    val futureId = Connection.db.run(queryDescription)
+
+    futureId.onComplete {
+      case Success(_) => println(s"QUery was successful to add actors")
+      case Failure(ex) => println(s"Faile with error $ex")
+
+    }
+    Thread.sleep(5000)
+  }
 
   def demoReallAllMovies(): Unit = {
 
-    val resultFuture: Future[Seq[Movie]] = Connection.db.run(SlickTables.movieTable.result) //select * from movieTable
+    val resultFuture: Future[Seq[Movie]] =
+      Connection.db.run(SlickTables.movieTable.result) //select * from movieTable
     resultFuture.onComplete {
       case Success(movies) => println(s"Fetched ${movies.mkString(", ")}")
       case Failure(ex) => println(s"Error fetching:  $ex")
     }
-    Thread.sleep(10000)
+    Thread.sleep(5000)
   }
 
   def demoReadSomeMovies(): Unit = {
-     //select * from movies where name like "Matrix"
-    val resultFuture: Future[Seq[Movie]] = Connection.db.run(SlickTables.movieTable.filter(movie => movie.name.like("%Matrix%")).result) //select * from movieTable
+    //select * from movies where name like "Matrix"
+    val resultFuture: Future[Seq[Movie]] = Connection.db.run(
+      SlickTables.movieTable.filter(movie => movie.name.like("%Matrix%")).result
+    ) //select * from movieTable
     resultFuture.onComplete {
       case Success(movies) => println(s"Fetched ${movies.mkString(", ")}")
       case Failure(ex) => println(s"Error fetching:  $ex")
     }
-    Thread.sleep(10000)
+    Thread.sleep(5000)
   }
 
+  def demoUpdateMovie(): Unit = {
 
-  def demoUpdateMovie() :  Unit = {
-
-    val queryDescriptor = SlickTables.movieTable.filter(movie => movie.id === 1L).update(shawShankRedemption.copy(lengthInMin = 150))
+    val queryDescriptor = SlickTables.movieTable
+      .filter(movie => movie.id === 1L)
+      .update(shawShankRedemption.copy(lengthInMin = 150))
     val futureId: Future[Int] = Connection.db.run(queryDescriptor)
 
     futureId.onComplete {
@@ -68,15 +86,52 @@ object Main {
       case Failure(ex) => println(s"Faile with error $ex")
 
     }
-    Thread.sleep(10000)
+    Thread.sleep(5000)
 
   }
 
-  def demoDeleteMovie() : Unit = {
+  def demoDeleteMovie(): Unit = {
     val queryDescriptor = SlickTables.movieTable.filter(movie => movie.id === 2L).delete
     Connection.db.run(queryDescriptor)
-    Thread.sleep(10000)
+    Thread.sleep(5000)
 
+  }
+
+  def demoReadMovieBySqlQuery(): Future[Vector[Movie]] = {
+
+    implicit val getResultMovie: GetResult[Movie] = {
+      //<< will parse the properties of Movie
+      GetResult(positionedResult =>
+        Movie(
+          positionedResult.<<,
+          positionedResult.<<,
+          LocalDate.parse(positionedResult.nextString()), //There is no custom parser by <<
+          positionedResult.<<))
+    }
+    val query = sql"""select * from movies."Movie"""".as[Movie]
+    Connection.db.run(query)
+
+  }
+
+  def multipleQueriesSingleTransaction(): Unit = {
+
+    val insertMovie = SlickTables.movieTable += theMatrix2
+    val insertActor = SlickTables.actorTable += redionXhepa2
+
+    val finalQuery = DBIO.seq(insertMovie, insertActor)
+
+    Connection.db.run(finalQuery.transactionally) //transactionally if one fails all rolls back
+
+  }
+
+  def findAllActorsByMovie(movieId: Long): Future[Seq[Actor]] = {
+    val joinQuery = SlickTables.actorMovieMappingTable
+      .filter(_.movieId === movieId)
+      .join(SlickTables.actorTable)
+      .on(_.actorId === _.id)
+      .map(_._2)
+    //select * from movieActorMapping m join actorTable a   on m.actorId == a.id
+    Connection.db.run(joinQuery.result)
   }
 
   def main(args: Array[String]): Unit = {
@@ -84,8 +139,17 @@ object Main {
     //demoReallAllMovies()
     //demoReadSomeMovies()
     //demoUpdateMovie()
-    demoDeleteMovie()
-  }
+    //demoDeleteMovie()
+    //demoInsertActor()
 
+    findAllActorsByMovie(1L).onComplete {
+      case Success(result) => println(result)
+      case Failure(ex) => println((s"Error happened: $ex"))
+    }
+
+    Thread.sleep(5000)
+    PrivateExecutionContext.executor.shutdown()
+
+  }
 
 }
--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -3,6 +3,9 @@ package org.slick.tutorial
 import java.time.LocalDate
 
 case class Movie(id: Long, name: String, releaseDate: LocalDate, lengthInMin: Int)
+case class Actor (id: Long, name: String)
+
+case class MovieActorMapping(id: Long, movieId: Long, actorId: Long)
 
 
 object SlickTables {
@@ -21,4 +24,33 @@ object SlickTables {
 
   // API entry point
   lazy val movieTable = TableQuery[MovieTable]
+
+
+
+  class ActorTable(tag: Tag) extends Table[Actor](tag, Some("movies"), "Actor") {
+    def id = column[Long]("actor_id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+
+
+    // mapping function to the case class, constuctor and destructor provided
+    override def * = (id,name)<>(Actor.tupled,Actor.unapply)
+  }
+
+  // API entry point
+  lazy val actorTable = TableQuery[ActorTable]
+
+
+  class MovieActorMappingTable(tag: Tag) extends Table[MovieActorMapping](tag, Some("movies"), "MovieActorMapping") {
+    def id = column[Long]("movie_actor_id", O.PrimaryKey, O.AutoInc)
+    def movieId = column[Long]("movie_id")
+    def actorId = column[Long]("actor_id")
+
+
+    // mapping function to the case class, constuctor and destructor provided
+    override def * = (id,movieId,actorId)<>(MovieActorMapping.tupled,MovieActorMapping.unapply)
+  }
+
+  // API entry point
+  lazy val actorMovieMappingTable = TableQuery[MovieActorMappingTable]
+
 }
